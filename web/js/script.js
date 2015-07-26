eb41d58f8a896fd02584415659105ff59d48dbc0
pr0dd <sgx4321@gmail.com>
2015-07-26 10:14:34 +0300

    Innitial commit

---
+++ web/js/script.js
@@ -0,0 +1,16 @@
+﻿/*deal with body's size if necessary;
+(function(){
+	var fitSize = function(){
+		var body = document.body,
+			height = window.innerHeight, 
+			width = window.innerWidth;
+		body.style.width = width + "px";
+		body.style.height = height + "px";
+	}
+	fitSize();
+	document.body.onresize = function(){
+		fitSize();
+	}
+})();
+*/
+
