70e2ea000b91e25e879a51705cd491f867ca7140
pr0dd <sgx4321@gmail.com>
2015-07-28 17:29:10 +0300

    header is ready

--- web/js/script.js
+++ web/js/script.js
@@ -1,1 +1,17 @@
-﻿
+﻿$(function(){
+	function header(){
+		var btn = $(".header button"),
+				tm = $(".top-menu nav"),
+				win = $(window),
+				doc = $(document);
+		btn.click(function(){
+			tm.slideToggle();
+		});
+		win.resize(function(){
+			if(doc.width()>600){
+				tm.slideDown();
+			}
+		});
+	}
+	header();
+});
