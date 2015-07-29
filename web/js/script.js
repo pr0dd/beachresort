f9afdc9a55ff5de88208b191e153d517ba0f3f59
pr0dd <sgx4321@gmail.com>
2015-07-29 20:03:49 +0300

    slider is ready to merge in master

--- web/js/script.js
+++ web/js/script.js
@@ -1,9 +1,13 @@
 ﻿$(function(){
+	//MENU BUTTON
 	function header(){
+		//intialize vars
 		var btn = $(".header button"),
 				tm = $(".top-menu nav"),
 				win = $(window),
 				doc = $(document);
+
+		//set event handlers
 		btn.click(function(){
 			tm.slideToggle();
 		});
@@ -14,4 +18,61 @@
 		});
 	}
 	header();
+	
+	//SLIDER
+	function slider(){
+		//settings for slider
+    var width = 100,
+    		animationSpeed = 800,
+    		pause = 4500,
+    		currentSlide = 1;
+
+    //cache DOM elements
+    var slider = $('.slider'),
+    		slideContainer = $('.move', slider),
+    		slides = $('img', slider);
+
+    //initialize interval
+    var interval;
+    
+    //start function
+    function startSlider() {
+      interval = setInterval(function() {
+        slideContainer.animate({'left': '-='+width+"%"}, animationSpeed, function() {
+          if (++currentSlide === slides.length) {
+            currentSlide = 1;
+            slideContainer.css('left', 0+"%");
+          }
+        });
+      }, pause);
+    }
+    
+    //pause function
+    function pauseSlider() {
+      clearInterval(interval);
+    }
+    
+    //set event handlers
+    slideContainer
+      .on('mouseenter', pauseSlider)
+      .on('mouseleave', startSlider);
+    
+    //show caption on hover
+  	function showCap(){
+			var images = $(".move img"),
+					cap = $(".slider .capture");
+			images
+				.on("mouseenter", function(){
+					var attrText = $(this).attr("data-cap");
+					cap.text(attrText).slideDown();
+				})
+				.on("mouseleave", function(){
+					cap.slideUp();
+				});
+		}
+		showCap();
+    startSlider();
+	}
+	slider();
+
 });
